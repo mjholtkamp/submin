4373152dfdff5ed2446bcf3bf90e2584f42ac022
Jean-Paul van Oosten <submin@jpvanoosten.nl>
2007-04-08 21:02:53 +0000

    * Added cookie handling in Request  * Added CGIGet to cgirequest module
    Todo (for cgirequest module):
     * post variables for CGI
    
    git-svn-id: https://ssl.supermind.nl/collab/svn/submin/trunk@80 9dfea958-5e0b-0410-aa7e-b09452dc4efc

--- dispatch/cgirequest.py
+++ dispatch/cgirequest.py
@@ -1,7 +1,8 @@
 import os
 import sys
+import cgi
 
-from request import Request
+from request import Request, GetVariables
 
 class CGIRequest(Request):
 	def __init__(self):
@@ -11,13 +12,14 @@ class CGIRequest(Request):
 		self.__output = sys.stdout
 		
 		self.post = {}
-		self.get = {}
+		self.get = CGIGet(self.__environ.get('QUERY_STRING'))
 		self.__incookie.load(self.__environ.get('HTTP_COOKIE', '')) 
 		self.path_info = self.__environ.get('PATH_INFO', '')
 		self.remote_address = self.__environ.get('REMOTE_ADDR')
 	
-	def writeHeaders(self):
-		raise NotImplementedError
-	
 	def write(self, content):
-		self.__output.write(content);
+		self.__output.write(content)
+
+class CGIGet(GetVariables):
+	def __init__(self, query_string):
+		self.variables = cgi.parse_qs(query_string)
--- dispatch/request.py
+++ dispatch/request.py
@@ -1,6 +1,8 @@
 from Cookie import SimpleCookie
 
 class Request(object):
+	"""Provide a consistent way to treat a Request."""
+	
 	def __init__(self):
 		self.post = {}
 		self.get = {}
@@ -18,6 +20,19 @@ class Request(object):
 		for header, value in headers.iteritems():
 			self.setHeader(header, value)
 	
+	def setCookieHeaders(self):
+		for name in self.__outcookies.keys():
+			path = self.__outcookies[name].get('path')
+			if path:
+				path = path.replace(' ', '%20') \
+						.replace(';', '%3B') \
+						.replace(',', '%3C')
+			self.__outcookies[name]['path'] = path
+
+		cookies = self.__outcookies.output(header='')
+		for cookie in cookies.splitlines():
+			self.setHeader('Set-Cookie', cookie.strip())
+	
 	def write(self, content):
 		raise NotImplementedError
 		
@@ -25,14 +40,34 @@ class Request(object):
 		self.write('Status: %d\r\n' % statusCode)
 
 	def writeHeaders(self):
+		self.setCookieHeaders()
 		for header, value in self.headers.iteritems():
 			self.write('%s: %s\r\n' % (header, value))
 		self.write('\r\n');
+	
+	def setCookie(self, key, value, path='/', expires=None):
+		self.__outcookies[key] = value
+		self.__outcookies[key]['path'] = path
+		if expires:
+			self.__outcookies[key]['expires'] = expires
+	
+	def getCookie(self, key, default=None):
+		try:
+			value = self.__incookies[key]
+		except KeyError:
+			return default
+			
+		if not value.value:
+			return default
+		
+		return value.value
 		
 class NoneObject:
 	pass
 
 class GetVariables(object):
+	"""Provide a consistent way to access the GET variables."""
+	
 	def __init__(self, query_string=''):
 		"""This is the method that needs to be overridden in CGIGet and 
 		ModPythonGet. Each uses a different way to parse the query string"""
