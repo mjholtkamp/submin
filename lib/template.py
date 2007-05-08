ea1811883d9fe89bfc15b3929ea51224a43b1ede
Michiel Holtkamp <git@supermind.nl>
2007-05-08 21:53:47 +0000

    * added [val] tag (closes #25)  * added [set:varname] tag (closes #26)
    git-svn-id: https://ssl.supermind.nl/collab/svn/submin/trunk@88 9dfea958-5e0b-0410-aa7e-b09452dc4efc

--- lib/template.py
+++ lib/template.py
@@ -43,10 +43,11 @@ def _split_command(thing):
 	return command, text
 
 
-def markup(text, meta=None, outermost=True):
+def markup(text, meta=None, localvars={}, outermost=True):
 	""" Markup language translator.
 		Markup language is less ugly than html, a la wiki.
 		Returns html text, modifies meta if given.
+		localvars is a dictionary containing localvars
 
 		If the result will be used by another call to markup with
 		the same meta, set outermost to False.
@@ -90,13 +91,13 @@ def markup(text, meta=None, outermost=True):
 			    text = u''
 			else:
 			    try:
-					text = getattr(this_module, 'markup_'+command)(text, meta, **thing_context)
+					cmdargs = command.split(':')
+					text = getattr(this_module, 'markup_'+cmdargs[0])(cmdargs[1:], text, meta, localvars, **thing_context)
 			    except AttributeError:
 					import StringIO, traceback
 					string_file = StringIO.StringIO()
 					traceback.print_exc(file=string_file)
 					text = u'<b><pre>'+quote_html(string_file.getvalue())+u'</pre></b>'
-
 			stack[-1] = stack[-1] + text
 		else:
 			if chunk in (u'\\]',u'\\[',u'\\\\'):
@@ -131,10 +132,16 @@ def make_callback_mark(meta, callback):
 	meta['_callbacks'] = meta.get('_callbacks',[]) + [(mark, callback)]
 	return mark
 
-def markup_val(text, meta=None, **varargs):
-	return text
+def markup_set(args, text, meta=None, localvars={}, **varargs):
+	localvars[args[0]] = text
+	return ''
 
-def markup_include(text, meta=None, **varargs):
+def markup_val(args, text, meta=None, localvars={}, **varargs):
+	if localvars.has_key(text):
+		return localvars[text]
+	return ''
+
+def markup_include(args, text, meta=None, localvars={}, **varargs):
 	import os
 	from os import path
 
@@ -151,7 +158,7 @@ def markup_include(text, meta=None, **varargs):
 	if fd != None:
 		lines = ''.join(fd.readlines())
 		if len(lines) > 0:
-			lines = markup(lines[:-1])
+			lines = markup(lines[:-1], localvars=localvars)
 
 		fd.close()
 
@@ -161,12 +168,13 @@ def markup_include(text, meta=None, **varargs):
 	return lines
 
 def test():
-	template="""[include ../templates/test]
-	{set:test 3}
-	[val test]
-	{iter:a some html {ival} code here<br />} 
-	{test:test xhtml code (test)<br />}
-	{else xhtml code here<br />}"""
+	template="""[set:test2 5][include ../templates/test][set:test 3]here be dragons
+[val test]
+[val test2]
+This will give no error or warning: [val test3]
+{iter:a some html {ival} code here<br />} 
+{test:test xhtml code (test)<br />}
+{else xhtml code here<br />}"""
 
 	print markup(template)
 
