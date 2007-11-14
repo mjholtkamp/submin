b3e2c33501273a440ee4432f72bcc4265abe9b52
Michiel Holtkamp <git@supermind.nl>
2007-11-14 23:12:10 +0000

    * used getStyle, so we don't have to mix css/inline styles!
    git-svn-id: https://ssl.supermind.nl/collab/svn/submin/trunk@150 9dfea958-5e0b-0410-aa7e-b09452dc4efc

--- www/js/ajax.js
+++ www/js/ajax.js
@@ -12,11 +12,32 @@ function AjaxPostRequest(url, body) {
 	return transport.responseText;
 }
 
+// from: http://www.elektronaut.no/articles/2006/06/07/computed-styles
+// Get the computed css property
+function getStyle( element, cssRule )
+{
+	if (document.defaultView && document.defaultView.getComputedStyle) {
+		var value = document.defaultView.getComputedStyle(element, '').getPropertyValue(
+			cssRule.replace( /[A-Z]/g, function(match, char) {
+				return "-" + char.toLowerCase();
+			} )
+		);
+	}
+	else if (element.currentStyle) var value = element.currentStyle[cssRule];
+	else                           var value = false;
+	return value;
+}
+
 function Log(message) {
 	if (message == "") {
 		return;
 	}
-	log = $c('div', {className: 'log', id: 'log', innerHTML: message, style: {top: "10px", height: "100px"}})
+	log = $c('div', {className: 'log', id: 'log', innerHTML: message})
+
+	top = getStyle(log, 'top')
+	log.style.top = top
+	height = getStyle(log, 'height')
+	log.style.height = height
 	document.body.appendChild(log)
 	setTimeout("RemoveLog()", 2000)
 }
