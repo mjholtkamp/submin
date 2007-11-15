197e7db23812646ade020de8a301aceb7b694b42
Michiel Holtkamp <git@supermind.nl>
2007-11-15 01:11:52 +0000

    * a bit different layout  * remove old logs/timeouts if they are still present
    git-svn-id: https://ssl.supermind.nl/collab/svn/submin/trunk@153 9dfea958-5e0b-0410-aa7e-b09452dc4efc

--- www/js/ajax.js
+++ www/js/ajax.js
@@ -36,10 +36,20 @@ function getStyle(element, cssRule)
 	return value;
 }
 
+var Log_timeout;
+
 function Log(message) {
 	if (message == "") {
 		return;
 	}
+
+	// first remove previous logs
+	log = document.getElementById('log')
+	if (log != null) {
+		clearTimeout(Log_timeout)
+		document.body.removeChild(log)
+	}
+
 	log = $c('div', {className: 'log', id: 'log', innerHTML: message})
 
 	document.body.appendChild(log)
@@ -52,7 +62,7 @@ function Log(message) {
 	log.style.opacity = value/10;
 	log.style.filter = 'alpha(opacity=' + value*10 + ')';
 
-	setTimeout("RemoveLog()", 2000)
+	Log_timeout = setTimeout("RemoveLog()", 2000)
 }
 
 function RemoveLog() {
@@ -61,7 +71,7 @@ function RemoveLog() {
 	height = parseInt(log.style.height)
 	if (_top + height > 0) {
 		log.style.top = "" + (_top - 5) + "px";
-		setTimeout("RemoveLog()", 10)
+		Log_timeout = setTimeout("RemoveLog()", 10)
 	} else {
 		document.body.removeChild(log)
 	}
