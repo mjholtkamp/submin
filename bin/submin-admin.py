44fa8049d13f96050213338061cb8ad19735e6b4
Michiel Holtkamp <git@supermind.nl>
2007-11-28 22:35:55 +0000

    * improved framework of the installer (refs #50)
    git-svn-id: https://ssl.supermind.nl/collab/svn/submin/trunk@161 9dfea958-5e0b-0410-aa7e-b09452dc4efc

--- bin/submin-admin.py
+++ bin/submin-admin.py
@@ -2,36 +2,89 @@
 
 class Admin:
 	def __init__(self):
-		self.commands = {}
-		self.commands['create'] = ['Create a new submin environment',
-			"""create <param1> <param2>"""]
-		self.commands['help'] = ['This help (supply a command for more' +
-			' information)', 'help <command>']
+		self.vars = {}
+		self.vars['submin root'] = '/var/lib/submin/'
+		self.vars['svn dir'] = 'svn'
+		self.vars['trac dir'] = 'trac'
+
+	def _path(self, path):
+		if path[0] == '/':
+			return path
+
+		return self.vars['submin root'] + path
 
 	def run(self, argv):
 		if len(argv) < 2:
-			a.help(argv)
+			self.c_help(argv)
 			return
 
 		try:
-			command = getattr(self, argv[1])
+			command = getattr(self, 'c_' + argv[1])
 			command(argv)
 		except:
-			self.usage(argv)
+			print "Sorry, don't know command `%s', try help" % argv[1]
+			raise
 
-	def help(self, argv):
-		if len(argv) < 3 or not self.commands.has_key(argv[2]):
+	def c_create(self, argv):
+		"""Create a new submin environment
+create [<submin-root> <svn-dir> <trac-dir>]
+
+	<submin-root>\t- submin data dir (default: %(submin root)s)
+	\t\t  holds the files: htpasswd, authz, submin.conf
+	<svn-dir>\t- svn repository dir (default: %(svn dir)s)
+	<trac-dir>\t- trac projects dir (default: %(trac dir)s)
+"""
+		pass
+
+	def c_help(self, argv):
+		"""This help (supply a command for more information)
+help <command>"""
+
+		if len(argv) < 3:
 			self.usage(argv)
 			return
 
-		print "Usage: %s %s " % (argv[0], self.commands[argv[2]][1])
+		try:
+			doc = self.getlongdescription('c_' + argv[2])
+			vars = self.replacedvars()
+			print "Usage: %s %s " % (argv[0], doc % vars)
+		except:
+			raise
+
+	def replacedvars(self):
+		vars = self.vars.copy()
+		vars['svn dir'] = self._path(vars['svn dir'])
+		vars['trac dir'] = self._path(vars['trac dir'])
+		return vars
+
+	def getshortdescription(self, command):
+		doc = self.getdescription(command)
+		if doc == None:
+			return None
+		return doc.split('\n', 1)[0]
+
+	def getlongdescription(self, command):
+		doc = self.getdescription(command)
+		if doc == None:
+			return None
+		return doc.split('\n', 1)[1]
+
+	def getdescription(self, command):
+		try:
+			method = getattr(self, command)
+		except:
+			return None
+
+		return method.__doc__
 
 	def usage(self, argv):
-		commands = '<' + '|'.join(x for x in self.commands.iterkeys()) + '>'
-		print "Usage: %s %s" % (argv[0], commands)
+		commands = [x for x in dir(self) if x.startswith('c_')]
+		commandstr = '<' + '|'.join(x[2:] for x in commands) + '>'
+		print "Usage: %s %s" % (argv[0], commandstr)
 		print
-		for command, description in self.commands.iteritems():
-			print "\t%s\t- %s" % (command, description[0])
+		for command in commands:
+			c = command[2:]
+			print "\t%10s - %s" % (c, self.getshortdescription(command))
 		print
 
 if __name__ == "__main__":
