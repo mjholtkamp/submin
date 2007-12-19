beb4cfc222d0101073f9874c69e2e140a6e12961
Jean-Paul van Oosten <submin@jpvanoosten.nl>
2007-12-19 20:17:52 +0000

    * Verification of entered password entered.
    git-svn-id: https://ssl.supermind.nl/collab/svn/submin/trunk@214 9dfea958-5e0b-0410-aa7e-b09452dc4efc

--- www/js/users.js
+++ www/js/users.js
@@ -1,5 +1,11 @@
 load('dom')
 
+var old_load = window.onload;
+window.onload = function() {
+	if (old_load) old_load();
+	$('password_button').onclick = verifyPassword;
+}
+
 function sendEmail() {
 	var response = AjaxSyncPostRequest(document.location,
 		"ajax&" + "email=" + $('email').value)
@@ -7,9 +13,40 @@ function sendEmail() {
 	Log(response.responseText, response.status != 200)
 }
 
-function sendPassword() {
+var enteredPassword;
+
+function verifyPassword() {
+	enteredPassword = $('password').value;
+	if (!enteredPassword) {
+		Log('Please enter a password before pressing "change"', true);
+		return false;
+	}
+
+	$('password').value = '';
+	this.parentNode.firstChild.innerHTML = 'Verify';
+	this.onclick = checkPasswords;
+	$('password').focus();
+	return false;
+}
+
+function checkPasswords() {
+	if ($('password').value != enteredPassword) {
+		Log('Verification of password failed', true);
+		return false;
+	}
+
+	enteredPassword = '';
+	$('password').value = '';
+	this.parentNode.firstChild.innerHTML = 'Password';
+	this.onclick = verifyPassword;
+	this.blur();
+	sendPassword(enteredPassword);
+	return false;
+}
+
+function sendPassword(password) {
 	var response = AjaxSyncPostRequest(document.location,
-		"ajax&" + "password=" + $('password').value)
+		"ajax&" + "password=" + password)
 
 	if (!response.responseText) {
 		responseText = 'Server did report an error, but response text was empty'
