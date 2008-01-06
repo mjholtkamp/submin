3a8b9a0835129f65dcf9194fd45515650a62202d
Michiel Holtkamp <git@supermind.nl>
2008-01-06 16:19:05 +0000

    Added plus and minus buttons (instead of textual plus/minus)
    git-svn-id: https://ssl.supermind.nl/collab/svn/submin/trunk@264 9dfea958-5e0b-0410-aa7e-b09452dc4efc

--- www/js/users.js
+++ www/js/users.js
@@ -121,7 +121,11 @@ function add() {
 	remover.className = 'remover';
 	remover.onclick = remove;
 	remover.href = '#';
-	remover.appendChild(document.createTextNode('-'));
+	var img = document.createElement('img');
+	img.className = "remover";
+	img.src = media_url + '/img/min.gif';
+	remover.appendChild(document.createTextNode(' '));
+	remover.appendChild(img);
 	li.appendChild(remover);
 	document.getElementById('memberof').insertBefore(li, this.parentNode);
 
