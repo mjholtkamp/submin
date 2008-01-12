b903e20208a68132d7f8428fa3369a29842dffac
Michiel Holtkamp <git@supermind.nl>
2008-01-12 20:00:29 +0000

    Implement create partly (refs #73)  * some bugfixes  * 'create' now creates submin.conf, htpasswd and authz (with default user admin:admin)  * make trac-dir optional (because it's not implemented yet, and we don't want to force people)  * assume libraries are installed in /usr/lib/submin/lib
    git-svn-id: https://ssl.supermind.nl/collab/svn/submin/trunk@288 9dfea958-5e0b-0410-aa7e-b09452dc4efc

--- bin/submin-admin.py
+++ bin/submin-admin.py
@@ -8,10 +8,15 @@ class Admin:
 		self.vars['trac dir'] = 'trac'
 
 	def _path(self, path):
-		if path[0] == '/':
+		if len(path) == 0 or path[0] == '/':
 			return path
 
 		return self.vars['submin root'] + path
+
+	def _setpath(self, name, path):
+		if path[-1:] != '/':
+			path += '/'
+		self.vars[name] = path
 
 	def run(self, argv):
 		if len(argv) < 2:
@@ -25,16 +30,48 @@ class Admin:
 			print "Sorry, don't know command `%s', try help" % argv[1]
 			raise
 
+	def create_subminconf_from_template(self):
+		vars = self.replacedvars()
+		submin_conf = open('/usr/lib/submin/conf/submin.conf.example').read()
+		submin_conf = submin_conf.replace('/var/lib/submin/svn', vars['svn dir'])
+		submin_conf = submin_conf.replace('/var/lib/submin', vars['submin root'])
+		submin_conf_file = vars['submin root'] + 'submin.conf'
+		out = open(submin_conf_file, 'w')
+		out.write(submin_conf)
+		out.close()
+
 	def c_create(self, argv):
 		"""Create a new submin environment
-create [<submin-root> <svn-dir> <trac-dir>]
+create <submin-root> <svn-dir> [<trac-dir>]
 
 	<submin-root>\t- submin data dir (default: %(submin root)s)
 	\t\t  holds the files: htpasswd, authz, submin.conf
 	<svn-dir>\t- svn repository dir (default: %(svn dir)s)
 	<trac-dir>\t- trac projects dir (default: %(trac dir)s)
 """
-		pass
+		if len(argv) > 2:
+			self._setpath('submin root', argv[2])
+		if len(argv) > 3:
+			self._setpath('svn dir', argv[3])
+		if len(argv) < 4:
+			self.vars['trac dir'] = ''
+
+		vars = self.replacedvars()
+		submin_conf_file = vars['submin root'] + 'submin.conf'
+
+		# create dir
+		import os
+		os.mkdir(os.path.dirname(submin_conf_file))
+
+		# make submin.conf
+		self.create_subminconf_from_template()
+
+		# add an admin user and submin-admin group
+		from config.config import Config
+		conf = Config(submin_conf_file)
+		conf.htpasswd.add('admin', 'admin')
+		conf.authz.addGroup('submin-admin', ['admin'])
+		print 'created submin configuration with default user admin (password: admin)'
 
 	def c_help(self, argv):
 		"""This help (supply a command for more information)
@@ -89,7 +126,7 @@ help <command>"""
 
 if __name__ == "__main__":
 	from sys import argv, path
-	path.append('/usr/lib/submin/www/lib')
+	path.append('/usr/lib/submin/lib')
 
 	try:
 		a = Admin()
