35d53d7acdcdcc5732a2fd3b17f4589c9dc4d30f
Jean-Paul van Oosten <submin@jpvanoosten.nl>
2008-01-23 22:00:38 +0000

    Added TestCase for the Authz.save method.
    Also added more meaningful messages to the self.assert_s
    
    
    git-svn-id: https://ssl.supermind.nl/collab/svn/submin/trunk@335 9dfea958-5e0b-0410-aa7e-b09452dc4efc

--- lib/config/authz/tests-authz.py
+++ lib/config/authz/tests-authz.py
@@ -1,8 +1,9 @@
 import unittest
 import os
 import tempfile
+import time
 
-from authz import Authz
+from authz import *
 
 class InitTest(unittest.TestCase):
 	"Tests the initializer for the Authz-module"
@@ -17,14 +18,33 @@ class InitTest(unittest.TestCase):
 		os.unlink(self.filename)
 
 	def testCreateFile(self):
-		self.assert_(os.path.exists(self.filename))
+		self.assert_(os.path.exists(self.filename),
+				'File %s does not exist' % self.filename)
 
 	def testCreateGroupsSection(self):
-		# XXX: This actually tests several functions! (namely the init and the
-		# save function). Therefore we also need a strict save() test!
+		# XXX: This actually tests several functions!
+		# This test alone does an extra operation authz.save(), therefore we
+		# also need a strict save() test!
 		self.authz.save()
 		lines = open(self.filename).readlines()
-		self.assert_('[groups]\n' in lines)
+		self.assert_('[groups]\n' in lines, '[groups] section not created')
+
+class SaveTest(unittest.TestCase):
+	"Testcase for the save() method on the Authz-objects."
+
+	def testModificationTime(self):
+		tempdir = tempfile.gettempdir()
+		filename = os.path.join(tempdir, 'authz')
+		authz = Authz(filename)
+		begin = os.path.getmtime(filename)
+
+		print '\nTesting modification time. This takes at least 1.1 seconds'
+		# sleep required to up the modification-time, reported in seconds
+		time.sleep(1.1)
+		authz.save()
+		end = os.path.getmtime(filename)
+		self.assert_(end > begin, 'Modification time after save not past ' \
+				+ 'modification time before save (begin > end)')
 
 if __name__ == '__main__':
 	unittest.main()
