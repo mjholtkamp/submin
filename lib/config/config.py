b9be9cc6f3cebefc4a430f0249f6c20df9fb3e42
Michiel Holtkamp <git@supermind.nl>
2008-01-23 19:21:06 +0000

    Use 'SUBMIN_CONF' environment variable instead of hardcoded config file  * fixes #77  * raise Exceptions on special cases
    git-svn-id: https://ssl.supermind.nl/collab/svn/submin/trunk@319 9dfea958-5e0b-0410-aa7e-b09452dc4efc

--- lib/config/config.py
+++ lib/config/config.py
@@ -9,13 +9,29 @@ class Config:
 	authz = None
 	htpasswd = None
 
-	def __init__(self, filename='../conf/submin.conf'):
+	def __init__(self, filename=''):
+		if not filename:
+			import os
+			if not os.environ.has_key('SUBMIN_CONF'):
+				raise Exception('SUBMIN_CONF environment not found')
+			filename = os.environ['SUBMIN_CONF']
+
 		if not self.cp:
 			self.cp = ConfigParser.ConfigParser()
 			self.cp.read(filename)
 
 		if not self.authz:
-			self.authz = Authz(self.get('svn', 'authz_file'))
+			authz_file = ''
+			try:
+				authz_file = self.get('svn', 'authz_file')
+			except ConfigParser.NoSectionError, e:
+				raise Exception(
+					"Missing config section 'svn' in file %s" % filename)
+			except ConfigParser.NoOptionError, e:
+				raise Exception(
+					"Missing config option 'authz_file' in file %s" % filename)
+
+			self.authz = Authz(authz_file)
 		if not self.htpasswd:
 			self.htpasswd = HTPasswd(self.get('svn', 'access_file'))
 
