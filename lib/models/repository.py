4dcdb5760ff2deba4a7cb905ffa209b3b9c5f48c
Michiel Holtkamp <git@supermind.nl>
2008-01-24 16:58:11 +0000

    Show contents of repository
    git-svn-id: https://ssl.supermind.nl/collab/svn/submin/trunk@340 9dfea958-5e0b-0410-aa7e-b09452dc4efc

--- lib/models/repository.py
+++ lib/models/repository.py
@@ -13,8 +13,27 @@ class Repository(object):
 #		if self.name not in config.authz.groups():
 #			raise Repository.DoesNotExist
 
-		self.paths = self.config.authz.paths(self.name)
-		self.paths.sort()
+		self.authz_paths = self.config.authz.paths(self.name)
+		self.authz_paths.sort()
+		self.paths = self.getsubdirs()
+
+	def getsubdirs(self):
+		import pysvn
+		import os
+
+		client = pysvn.Client()
+		reposdir = self.config.get('svn', 'repositories')
+		url = 'file://' + reposdir
+		url = os.path.join(url, self.name)
+
+		files = client.ls(url, recurse=True)
+		dirs = []
+		for file in files:
+			if file['kind'] == pysvn.node_kind.dir:
+				name = file['name']
+				dirs.append(name[len(url):])
+
+		return dirs
 
 	def __str__(self):
 		return self.name
