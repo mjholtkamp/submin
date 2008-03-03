adb3c78a355defab74489f8bd1ff53ee897f3e15
Michiel Holtkamp <git@supermind.nl>
2008-03-03 20:58:19 +0000

    Don't use evaluate_main if user is not logged in
    git-svn-id: https://ssl.supermind.nl/collab/svn/submin/trunk@478 9dfea958-5e0b-0410-aa7e-b09452dc4efc

--- lib/views/error.py
+++ lib/views/error.py
@@ -1,10 +1,15 @@
 from template.shortcuts import evaluate_main
+from template import evaluate
 from dispatch.response import Response
 
 class ErrorResponse(Response):
 	def __init__(self, errormsg, request=None):
 		localvars = {}
 		localvars['errormsg'] = errormsg
-		formatted = evaluate_main('error.html', localvars, request=request)
+		if request and request.session and 'user' in request.session:
+			formatted = evaluate_main('error.html', localvars, request=request)
+		else:
+			formatted = evaluate('error.html', localvars)
+
 		Response.__init__(self, formatted)
 
