ee2621ff6b3d3859dfee73d2cb7bea10626a003c
Jean-Paul van Oosten <submin@jpvanoosten.nl>
2008-04-23 20:21:46 +0000

    Allow UIScenarios flags to be saved.
    You need to be able to write in the directory where your scenarios_file.txt is
    saved to be writable. UIScenarios uses that filename + ".saved" to write the
    flags.
    
    
    git-svn-id: https://ssl.supermind.nl/collab/svn/submin/submin/trunk@533 9dfea958-5e0b-0410-aa7e-b09452dc4efc

--- lib/models/uiscenarios.py
+++ lib/models/uiscenarios.py
@@ -1,16 +1,34 @@
+import cPickle
+from distutils.dep_util import newer
+
 from config.config import Config
 from path.path import Path
 
 class UIScenarios(object):
+	"""Create a list of sections with lists of options.
+	Needs a file with sections / options in this format:
+		A section is a tab-separated line, starting with "="
+		An option is a tab-separated line (the tab separatas table-cells)
+
+	Saves this list of sections to a pickled file, which is reset when the
+	file with option-texts is newer than the pickled file.
+	"""
 	def __init__(self, filename=None):
 		if filename is None:
 			filename = Config().get("tests", "scenarios_file")
 		self.filename = filename
-		self.sections = []
 
+		# Don't randomly change this order! Code below uses indexes to refer
+		# to these browsers and does not rebuild those indexes when getting
+		# data from pickled-file.
 		self.browsers = ["Safari 3", "Firefox 2", "Firefox 3", "IE 7"]
 
-		self._parse()
+		self.sections = []
+		# Should we get the sections from .txt file or pickled (".saved") file?
+		if newer(self.filename, self.filename + ".saved"):
+			self._parse()
+		else:
+			self.sections = self.load_state()
 
 	def _parse(self):
 		fp = open(self.filename)
@@ -23,4 +41,42 @@ class UIScenarios(object):
 						{"heading": line.lstrip("=").strip().split("\t"),
 							"options": []})
 			else:
-				self.sections[-1]["options"].append(line.strip().split("\t"))
+				optiondict = {}
+				optiondict["texts"] = line.strip().split("\t")
+				# Flags default to False
+				optiondict["flags"] = [False for i in self.browsers]
+				optiondict["hash"] = hash(line)
+				self.sections[-1]["options"].append(optiondict)
+
+	def clean_state(self):
+		"""Sets all flags to False for every option."""
+		for section in self.sections:
+			for option in section["options"]:
+				option["flags"] = [False for i in self.browsers]
+
+	def set_state(self, hash_value, flags):
+		# Currently only tested for CGI!
+		for section in self.sections:
+			for option in section["options"]:
+				if str(option["hash"]) != str(hash_value):
+					continue
+				try:
+					for flag in flags:
+						option["flags"][int(flag.value)] = True
+				except TypeError:
+					option["flags"][int(flags.value)] = True
+
+	def save_state(self, write=True):
+		fp = open(self.filename + ".saved", "wb")
+		cPickle.dump(self.sections, fp)
+		fp.close()
+
+	def load_state(self):
+		sections = []
+		try:
+			fp = open(self.filename + ".saved", "rb")
+			sections = cPickle.load(fp)
+			fp.close()
+		except:
+			pass
+		return sections
--- lib/views/uiscenarios.py
+++ lib/views/uiscenarios.py
@@ -8,9 +8,15 @@ class UIScenarios(View):
 	@admin_required
 	def handler(self, req, path):
 		uiscenarios = UIScenariosModel()
+		if req.post:
+			uiscenarios.clean_state()
+			for key in req.post:
+				uiscenarios.set_state(key, req.post[key])
+			uiscenarios.save_state()
 		localvars = {}
 		localvars["sections"] = uiscenarios.sections
 		localvars["browsers"] = uiscenarios.browsers
 
+		# Don't need sidebar
 		formatted = evaluate('tests/uiscenarios.html', localvars)
 		return Response(formatted)
