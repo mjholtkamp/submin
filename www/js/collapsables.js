9134c1d03145b04ab206f151bd3350d86d87a4ac
Michiel Holtkamp <git@supermind.nl>
2008-10-13 05:02:27 +0000

    Default don't display sidebar, let javascript make it visible (this removes annoying expand/collapse effect when switching pages
    git-svn-id: https://ssl.supermind.nl/collab/svn/submin/submin/trunk@568 9dfea958-5e0b-0410-aa7e-b09452dc4efc

--- www/js/collapsables.js
+++ www/js/collapsables.js
@@ -176,12 +176,12 @@ function collapsables_collapse(prefix, triggered, collapse)
 	if (collapse) {
 		collapsee.style.display = 'none';
 	} else {
-		collapsee.style.display = '';
+		collapsee.style.display = 'compact';
 	}
 
 	// force refresh on certain browsers
 	var root = collapsables_getRoot(prefix, triggered);
 	root.style.display = 'none';
-	root.style.display = '';
+	root.style.display = 'compact';
 }
 
