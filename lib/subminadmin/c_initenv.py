d70e8e5e987d1dba59e88c9011e7a03ae1952dcc
Michiel Holtkamp <git@supermind.nl>
2009-02-07 07:09:50 +0000

    Added stub of initenv command (refs #139)
    git-svn-id: https://ssl.supermind.nl/collab/svn/submin/submin/trunk@754 9dfea958-5e0b-0410-aa7e-b09452dc4efc

---
+++ lib/subminadmin/c_initenv.py
@@ -0,0 +1,12 @@
+class c_initenv():
+	'''Initialize a new enviroment
+Usage:
+    initenv           - start interactive console which will ask questions
+    initenv [options] - create environment without asking questions'''
+
+	def __init__(self, sa, argv):
+		self.sa = sa
+		self.argv = argv
+
+	def run(self):
+		pass
