ea1b0298adf394fb02c8f82a925f92039781f4ae
Michiel Holtkamp <git@supermind.nl>
2009-02-07 07:51:42 +0000

    initenv command now creates base directory structure (refs #139)
    git-svn-id: https://ssl.supermind.nl/collab/svn/submin/submin/trunk@755 9dfea958-5e0b-0410-aa7e-b09452dc4efc

--- lib/subminadmin/c_initenv.py
+++ lib/subminadmin/c_initenv.py
@@ -1,12 +1,58 @@
+import os
+
 class c_initenv():
 	'''Initialize a new enviroment
 Usage:
-    initenv           - start interactive console which will ask questions
-    initenv [options] - create environment without asking questions'''
+    initenv
+            -- Create environment interactively
+    initenv <repospath>
+            -- Create environment from arguments'''
 
 	def __init__(self, sa, argv):
 		self.sa = sa
 		self.argv = argv
+		self.defaults = {'svn dir': 'svn'}
+		self.init_vars = {'conf dir': 'conf'}
+
+	def prompt_user(self, prompt, default):
+		defval = self.defaults[default]
+		a = raw_input("%s [%s]> " % (prompt, defval))
+		if a == '':
+			return defval
+
+		return a
+
+	def interactive(self):
+		svn_dir = self.prompt_user("Path to the repository", 'svn dir')
+		self.init_vars['svn dir'] = svn_dir
+		self.create_env()
+
+	def create_dir(self, directory):
+		"""Create a relative or absulute directory, if it doesn't exists already"""
+		if not os.path.exists(directory):
+			if directory[0] != os.path.sep:
+				directory = os.path.join(self.sa.env, directory)
+			os.makedirs(directory, mode=0700)
+
+	def create_env(self):
+		"""This is called when all info is gathered"""
+		self.create_dir(self.sa.env)
+		self.create_dir(self.init_vars['svn dir'])
+		self.create_dir(self.init_vars['conf dir'])
 
 	def run(self):
-		pass
+		if os.path.exists(self.sa.env):
+			print "Directory already exists, won't overwrite"
+			return
+
+		if len(self.argv) < 1:
+			self.interactive()
+			return
+
+		expected_args = 1
+		if len(self.argv) != expected_args:
+			print "Wrong number of arguments, expected: %u" % expected_args
+			return
+
+		self.init_vars['svn dir'] = self.argv[0]
+		self.create_env()
