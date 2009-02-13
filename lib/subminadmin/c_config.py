d5bf63ecea706f5075f351614259383a1580476a
Michiel Holtkamp <git@supermind.nl>
2009-02-13 13:15:52 +0000

    Make subminadmin config use version 2 style config, implemented defaults(), implemented set()
    git-svn-id: https://ssl.supermind.nl/collab/svn/submin/submin/trunk@766 9dfea958-5e0b-0410-aa7e-b09452dc4efc

--- lib/subminadmin/c_config.py
+++ lib/subminadmin/c_config.py
@@ -1,5 +1,6 @@
 from path.path import Path
 from config.config import ConfigData
+import os
 
 class c_config():
 	'''Commands to change config
@@ -13,16 +14,64 @@ Usage:
 	def __init__(self, sa, argv):
 		self.sa = sa
 		self.argv = argv
+		os.environ['SUBMIN_ENV'] = self.sa.env
 
 	def subcmd_defaults(self, argv):
-		conf_path = str(Path(self.sa.env) + 'conf' + 'submin.ini')
-		# set defaults
+		filename = str(Path(self.sa.env) + 'conf' + 'submin.ini')
+		self.submin_ini_defaults(filename)
 
 	def subcmd_get(self, argv):
-		print argv
+		c = ConfigData()
 
 	def subcmd_set(self, argv):
-		print argv
+		if len(argv) != 3:
+			self.sa.execute(['help', 'config'])
+			return
+
+		c = ConfigData()
+		c.set(argv[0], argv[1], argv[2])
+		c.save()
+
+	def session_salt(self):
+		import random
+		salts = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789./'
+		salt = ''
+		rand = random.Random()
+		for i in range(16):
+			salt += rand.choice(salts)
+
+		return salt
+
+	def submin_ini_defaults(self, filename):
+		vars = {
+			'authz': 'authz',
+			'userprop': 'userprop.conf',
+			'htpasswd': 'htpasswd',
+			'svn dir': 'svn',
+			'http base': '',
+			'bin dir': 'bin',
+			'session salt': self.session_salt()
+		}
+
+		submin_ini = '''[svn]
+authz_file = %(authz)s
+userprop_file = %(userprop)s
+access_file = %(htpasswd)s
+repositories = %(svn dir)s
+
+[www]
+base_url = %(http base)s/submin
+svn_base_url = %(http base)s/svn
+trac_base_url = %(http base)s/trac
+
+[backend]
+bindir = %(bin dir)s
+
+[generated]
+session_salt = %(session salt)s
+''' % vars
+
+		file(filename, 'w').write(submin_ini)
 
 	def run(self):
 		if len(self.argv) < 1:
