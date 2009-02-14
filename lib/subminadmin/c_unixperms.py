5bf4c5e0bf13004baf62dcb3130e446a173bb893
Michiel Holtkamp <git@supermind.nl>
2009-02-14 03:28:49 +0000

    Fix permissions like this: set everything to root:www-user and then set certain files/directories to www-user:www-user, this way, if we forget anything we will notice
    git-svn-id: https://ssl.supermind.nl/collab/svn/submin/submin/trunk@782 9dfea958-5e0b-0410-aa7e-b09452dc4efc

--- lib/subminadmin/c_unixperms.py
+++ lib/subminadmin/c_unixperms.py
@@ -14,8 +14,12 @@ Usage:
 
 	def subcmd_fix(self, argv):
 		if os.getuid() != 0:
-			print '''To also set ownerships properly, execute as root:
+			print '''
+To also set ownerships properly, execute as root:
+
     submin-admin unixperms fix
+
+This should also remove possible following warnings.
 '''
 			self.root = False
 
@@ -30,39 +34,49 @@ Usage:
 		config = Config()
 
 		nonroots = []
-		nonroots.append(config.base_path)
+		nonroots.append(config.base_path + 'auth')
 		nonroots.append(config.getpath('trac', 'basedir'))
 		for filename in ['authz_file', 'access_file', 'userprop_file', 'repositories']:
 			 nonroots.append(config.getpath('svn', filename))
 
-		roots = []
-		roots.append(config.base_path + 'conf')
-		roots.append(config.base_path + 'conf' + 'submin.ini')
-		roots.append(config.getpath('backend', 'bindir'))
-
 		apache = self._apache_user(unixuser)
 		root = self._apache_user('root')
 
-		self._change_items(nonroots, apache)
-		self._change_items(roots, root)
+		self._recurse_change(str(config.base_path), root.pw_uid, apache.pw_gid)
+		for nonroot in nonroots:
+			item = str(nonroot)
+			self._recurse_change(item, apache.pw_uid, apache.pw_gid)
+			if os.path.isfile(item):
+				st = os.stat(os.path.dirname(item))
+				if st.st_uid != apache.pw_uid:
+					print '''\
+*** WARN: file should be writable by apache user, but parent directory is not.
+             (%s)''' % item
 
-	def _change_items(self, items, user):
-		for item in items:
-			s = str(item)
+	def _recurse_change(self, directory, user, group):
+		self._change_item(directory, user, group)
+		for root, dirs, files in os.walk(directory):
+			for f in files:
+				self._change_item(os.path.join(root, f), user, group)
+			for d in dirs:
+				path = os.path.join(root, d)
+				self._recurse_change(path, user, group)
+
+	def _change_item(self, item, user, group):
+		try:
+			if os.path.isdir(item):
+				os.chmod(item, 0750)
+			else:
+				os.chmod(item, 0640)
+		except OSError:
+			print ' *** Failed to change permissions of %s' % s
+			print '     Do you have the right permissions?'
+
+		if self.root:
 			try:
-				if os.path.isdir(s):
-					os.chmod(s, 0750)
-				else:
-					os.chmod(s, 0640)
+				os.chown(item, user, group)
 			except OSError:
-				print ' *** Failed to change permissions of %s' % s
-				print '     Do you have the right permissions?'
-
-			if self.root:
-				try:
-					os.chown(s, user.pw_uid, user.pw_gid)
-				except OSError:
-					print ' *** Failed to change ownership of %s' % s
+				print ' *** Failed to change ownership of %s' % s
 
 	def _apache_user(self, preferred=''):
 		from pwd import getpwnam
