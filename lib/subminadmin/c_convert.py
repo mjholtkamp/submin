58baa37cf5604e4d7fc9a704c1fec640a55a56bb
Michiel Holtkamp <git@supermind.nl>
2009-02-22 08:57:58 +0000

    submin-admin: 'convert' now also converts options (but not yet data) (refs #141)
    git-svn-id: https://ssl.supermind.nl/collab/svn/submin/submin/trunk@809 9dfea958-5e0b-0410-aa7e-b09452dc4efc

--- lib/subminadmin/c_convert.py
+++ lib/subminadmin/c_convert.py
@@ -10,6 +10,11 @@ Usage:
 	def __init__(self, sa, argv):
 		self.sa = sa
 		self.argv = argv
+		self.old_options = {}
+		self.oldc = None
+
+	def _get_options_from_config(self, section, option, store_as):
+		self.old_options[store_as] = self.oldc.get(section, option)
 
 	def convert(self, oldconfig):
 		# Because Config() is a singleton, we have to be careful with it.
@@ -18,12 +23,21 @@ Usage:
 		# before that. So, to make it easy on ourselves, we use ConfigData,
 		# which is not a Singleton
 		try:
-			oldc = ConfigData(oldconfig)
+			self.oldc = ConfigData(oldconfig)
 		except IOError, e:
 			print "Reading old config failed: %s" % str(e)
 			return
 
 		cmd_list = ['initenv', 'create_user=no']
+		
+		self._get_options_from_config('svn', 'repositories', 'svn_dir')
+		self._get_options_from_config('www', 'base_url', 'submin_url')
+		self._get_options_from_config('www', 'trac_base_url', 'trac_url')
+		self._get_options_from_config('www', 'svn_base_url', 'svn_url')
+		self._get_options_from_config('trac', 'basedir', 'trac_dir')
+		for (key, value) in self.old_options.iteritems():
+			cmd_list.append('%s=%s' % (key, value))
+
 		self.sa.execute(cmd_list)
 
 	def run(self):
