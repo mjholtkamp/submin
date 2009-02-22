3ac9d1c49b49ace8154134072c282b3fecdb2752
Michiel Holtkamp <git@supermind.nl>
2009-02-22 10:05:17 +0000

    submin-admin: 'convert' can also decide to copy svn/trac or not, depending on values in old config (refs #141)
    git-svn-id: https://ssl.supermind.nl/collab/svn/submin/submin/trunk@812 9dfea958-5e0b-0410-aa7e-b09452dc4efc

--- lib/subminadmin/c_convert.py
+++ lib/subminadmin/c_convert.py
@@ -1,6 +1,7 @@
 import os
 from config.config import ConfigData
 from path.path import Path
+import shutil
 
 class c_convert():
 	'''Create a new configuration from an old-style config
@@ -12,9 +13,32 @@ Usage:
 		self.argv = argv
 		self.old_options = {}
 		self.oldc = None
+		self.import_svn = False
+		self.import_trac = False
 
 	def _get_options_from_config(self, section, option, store_as):
 		self.old_options[store_as] = self.oldc.get(section, option)
+
+	def _check_imports(self):
+		"""Check if we should import svn and trac or just point to it. We
+do this by checking if they reside in the same dirs as the auth files."""
+
+		dirs = {}
+		dirs['authz_file'] = self.old_options['authz_file']
+		dirs['access_file'] = self.old_options['access_file']
+		dirs['userprop_file'] = self.old_options['userprop_file']
+		dirs['svn_dir'] = self.old_options['svn_dir']
+		dirs['trac_dir'] = self.old_options['trac_dir']
+
+		for d in dirs:
+			dirs[d] = os.path.dirname(dirs[d])
+
+		if dirs['authz_file'] == dirs['access_file'] and \
+				dirs['authz_file'] == dirs['userprop_file']:
+			if dirs['authz_file'] == dirs['svn_dir']:
+				self.import_svn = True
+			if dirs['authz_file'] == dirs['trac_dir']:
+				self.import_trac = True
 
 	def convert(self, oldconfig):
 		# Because Config() is a singleton, we have to be careful with it.
@@ -29,16 +53,56 @@ Usage:
 			return
 
 		cmd_list = ['initenv', 'create_user=no']
-		
+
+		# get options that we need to pass to initenv
 		self._get_options_from_config('svn', 'repositories', 'svn_dir')
 		self._get_options_from_config('www', 'base_url', 'submin_url')
 		self._get_options_from_config('www', 'trac_base_url', 'trac_url')
 		self._get_options_from_config('www', 'svn_base_url', 'svn_url')
 		self._get_options_from_config('trac', 'basedir', 'trac_dir')
-		for (key, value) in self.old_options.iteritems():
+
+		# copy this list, because we don't want the other options
+		initenv_options = self.old_options.copy()
+
+		# get some more options, we need later on
+		self._get_options_from_config('svn', 'authz_file', 'authz_file')
+		self._get_options_from_config('svn', 'access_file', 'access_file')
+		self._get_options_from_config('svn', 'userprop_file', 'userprop_file')
+		self._get_options_from_config('svn', 'repositories', 'svn_dir')
+		self._get_options_from_config('trac', 'basedir', 'trac_dir')
+
+		# now see if we need to change options
+		self._check_imports()
+		if (self.import_svn):
+			initenv_options['svn_dir'] = 'svn'
+		if (self.import_trac):
+			initenv_options['trac_dir'] = 'trac'
+		
+		# now finally build the options list
+		for (key, value) in initenv_options.iteritems():
 			cmd_list.append('%s=%s' % (key, value))
 
-		self.sa.execute(cmd_list)
+		# call initenv
+		if not self.sa.execute(cmd_list):
+			return
+
+		# we now have our environment, so we can create a config object
+		os.environ['SUBMIN_ENV'] = self.sa.env
+		newc = ConfigData()
+
+		# copy svn/trac data, if necessary
+		new_svn_dir = str(newc.getpath('svn', 'repositories'))
+		new_trac_dir = str(newc.getpath('trac', 'basedir'))
+		if self.import_svn:
+			self._copy_dir(self.old_options['svn_dir'], new_svn_dir)
+		if self.import_trac:
+			self._copy_dir(self.old_options['trac_dir'], new_trac_dir)
+
+		# copy users/groups/permissions
+
+	def _copy_dir(self, old_dir, new_dir):
+		os.rmdir(new_dir) # copytree will fail otherwise
+		shutil.copytree(old_dir, new_dir)
 
 	def run(self):
 		if len(self.argv) != 1:
