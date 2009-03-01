c0e26050b88bdff5f4f20ae7918c2ed37dd54d3a
Michiel Holtkamp <git@supermind.nl>
2009-03-01 09:42:31 +0000

    Refactored config.py a bit, because we want to seperate some functionality later on (to have different auth modules)
    git-svn-id: https://ssl.supermind.nl/collab/svn/submin/submin/trunk@816 9dfea958-5e0b-0410-aa7e-b09452dc4efc

--- lib/config/config.py
+++ lib/config/config.py
@@ -38,31 +38,57 @@ class ConfigData:
 			return 0
 
 	def reinit(self):
-		if os.environ.has_key('SUBMIN_ENV'):
-			self.version = 2
-			self.base_path = Path(os.environ['SUBMIN_ENV'])
-			self.filename = str(self.base_path + 'conf' + 'submin.ini')
-			self.use_env = False
-			self.template_path = self.base_path + 'static' + 'templates'
+		if os.environ.has_key('SUBMIN_ENV') or os.path.isdir(self.filename):
+			self.reinit_version2()
+		else:
+			self.reinit_version1()
 
-		filename = self.filename
+	def reinit_version1(self):
 		if self.use_env:
 			if not os.environ.has_key('SUBMIN_CONF'):
 				raise Exception('SUBMIN_CONF environment not found')
+			oldfilename = self.filename
 			self.filename = os.environ['SUBMIN_CONF']
-			if filename != self.filename:
+			if oldfilename != self.filename:
 				self.ctimes["config"] = 0
 				self.ctimes["authz"] = 0
 				self.ctimes["userprop"] = 0
 				self.ctimes["htpasswd"] = 0
-				filename = self.filename
-		
+
+		self.reread_config_file()
+		self.reread_authz_files()
+
+		# Normalize base_url and make it a member for easy access
+		self.base_url = Path(self.get("www", "base_url"), absolute=True, append_slash=True)
+
+		if self.version == 1:
+			if os.environ.has_key('SCRIPT_FILENAME'):
+				p = os.path.dirname(os.path.realpath(os.environ['SCRIPT_FILENAME']))
+				self.base_path = Path(os.path.dirname(p.rstrip('/')))
+			else:
+				# no cgi script
+				self.base_path = Path('')
+			self.template_path = self.base_path + 'templates'
+
+	def reinit_version2(self):
+		self.version = 2
+		self.base_path = Path(os.environ['SUBMIN_ENV'])
+		self.filename = str(self.base_path + 'conf' + 'submin.ini')
+		self.template_path = self.base_path + 'static' + 'templates'
+
+		self.reread_config_file()
+		self.reread_authz_files()
+
+		# Normalize base_url and make it a member for easy access
+		self.base_url = Path(self.get("www", "base_url"), absolute=True, append_slash=True)
+
+	def reread_config_file(self):
 		try:
 			conf_stat = os.stat(self.filename)
 		except OSError, e:
 			raise CouldNotReadConfig(str(e))
 
-		conf_ctime = conf_stat.st_ctime 
+		conf_ctime = conf_stat.st_ctime
 		if not self.cp or conf_ctime > self.ctimes["config"]:
 			self.cp = ConfigParser.ConfigParser()
 			files = self.cp.read(self.filename)
@@ -71,27 +97,29 @@ class ConfigData:
 
 			self.ctimes["config"] = conf_ctime
 
+	def reread_authz_files(self):
 		# authz/userprop preparation
 		authz_file = ''
 		userprop_file = ''
+
 		try:
 			authz_file = str(self.getpath('svn', 'authz_file'))
 		except ConfigParser.NoSectionError, e:
 			raise Exception(
-				"Missing config section 'svn' in file %s" % filename)
+				"Missing config section 'svn' in file %s" % self.filename)
 		except ConfigParser.NoOptionError, e:
 			raise Exception(
-				"Missing config option 'authz_file' in file %s" % filename)
+				"Missing config option 'authz_file' in file %s" % self.filename)
 
 		try:
 			userprop_file = str(self.getpath('svn', 'userprop_file'))
 		except ConfigParser.NoSectionError, e:
 			raise Exception(
-				"Missing config section 'svn' in file %s" % filename)
+				"Missing config section 'svn' in file %s" % self.filename)
 		except ConfigParser.NoOptionError, e:
 			raise Exception(
 				"Missing config option 'userprop_file' in file %s" % \
-						filename)
+						self.filename)
 
 		authz_ctime = self._ctime(authz_file)
 		refresh_authz = authz_ctime > self.ctimes["authz"]
@@ -109,18 +137,6 @@ class ConfigData:
 		if not self.htpasswd or htpasswd_ctime > self.ctimes["htpasswd"]:
 			self.htpasswd = HTPasswd(htpasswd_file)
 			self.ctimes["htpasswd"] = htpasswd_ctime
-
-		# Normalize base_url and make it a member for easy access
-		self.base_url = Path(self.get("www", "base_url"), absolute=True, append_slash=True)
-
-		if self.version == 1:
-			if os.environ.has_key('SCRIPT_FILENAME'):
-				p = os.path.dirname(os.path.realpath(os.environ['SCRIPT_FILENAME']))
-				self.base_path = Path(os.path.dirname(p.rstrip('/')))
-			else:
-				# no cgi script
-				self.base_path = Path('')
-			self.template_path = self.base_path + 'templates'
 
 	def getpath(self, section, variable):
 		path = Path(self.get(section, variable))
