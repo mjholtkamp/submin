4692a7853c5ebd9954f80d064fd1a14898a8d1d1
Michiel Holtkamp <git@supermind.nl>
2009-04-18 15:01:29 +0000

    Removed double code
    git-svn-id: https://ssl.supermind.nl/collab/svn/submin/submin/trunk@844 9dfea958-5e0b-0410-aa7e-b09452dc4efc

--- lib/unicode/__init__.py
+++ lib/unicode/__init__.py
@@ -26,34 +26,6 @@ def url_uc_decode(s):
 		s = unicode(s, 'unicode_escape')
 
 	return re.sub('%u[a-zA-Z0-9]{4,4}', _url_uc_to_uc_callback, s)
-# -*- coding: utf-8 -*-
-"""Tools to convert from and to unicode"""
-
-import re
-
-def uc_str(obj, encoding='utf-8', errors='replace'):
-	if isinstance(obj, str):
-		return unicode(obj, encoding, errors)
-	if isinstance(obj, unicode):
-		return obj
-
-	return unicode(str(obj), encoding, errors)
-
-def _url_uc_to_uc_callback(matchobj):
-	"""Convert characters to utf-8"""
-	s = matchobj.group(0).encode('utf-8')
-	u = s.replace("%", "\\")
-	return unicode(u, 'unicode_escape')
-
-def url_uc_decode(s):
-	"""Convert expressions like '%uFFFF' to '\\uFFFF' and encode as unicode.
-	This way of encoding unicode in urls is often used by javascript."""
-	try:
-		s = unicode(s, 'utf-8')
-	except: # sometimes this happens... non-unicode encoded as unicode?
-		s = unicode(s, 'unicode_escape')
-
-	return re.sub('%u[a-zA-Z0-9]{4,4}', _url_uc_to_uc_callback, s)
 
 def uc_from_svn(path):
 	"""Copied from Trac"""
