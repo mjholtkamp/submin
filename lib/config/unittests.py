c3c8f89518945dd811915ae0752faec866ae1212
Michiel Holtkamp <git@supermind.nl>
2009-04-20 13:06:27 +0000

    Code cleanup: unittests Config class, also added more checks
    git-svn-id: https://ssl.supermind.nl/collab/svn/submin/submin/trunk@875 9dfea958-5e0b-0410-aa7e-b09452dc4efc

--- lib/config/unittests.py
+++ lib/config/unittests.py
@@ -1,15 +1,18 @@
 import unittest
-from config import Config, ConfigData
+from config import Config, ConfigData, MissingConfigData, CouldNotReadConfig
 import os
 import tempfile
 
-base = "/tmp/submin-unittest"
-authz_file = "%s/conf/svn.authz" % base
-access_file = "%s/conf/htpasswd" % base
-userprop_file = "%s/conf/userproperties.ini" % base
-repositories = "%s/svn/" % base
+class SingletonTest(unittest.TestCase):
+	"""Only tests the 'singletonness' of the Config module"""
+	def setUp(self):
+		self.base = tempfile.mkdtemp()
+		self.authz_file = "%s/conf/svn.authz" % self.base
+		self.access_file = "%s/conf/htpasswd" % self.base
+		self.userprop_file = "%s/conf/userproperties.ini" % self.base
+		self.repositories = "%s/svn/" % self.base
 
-conf_data = """[svn]
+		conf_data = """[svn]
 authz_file = %s
 access_file = %s
 userprop_file = %s
@@ -19,18 +22,11 @@ repositories = %s
 base_url = /
 
 [generated]
-session_salt = 1984500a.cjctec.8""" % (authz_file, access_file, userprop_file, repositories)
-
-authz_content = ""
-access_content = "admin:$1$9BEYXPLN$UtPfDFp1XmyXO0NuMStTP."
-userprop_content = ""
-
-class SingletonTest(unittest.TestCase):
-	"""Only tests the 'singletonness' of the Config module"""
-	def setUp(self):
-		tempdir = tempfile.gettempdir()
-		self.filename = os.path.join(tempdir, 'submin.conf')
-		fp = open(self.filename, 'a+')
+session_salt = 1984500a.cjctec.8""" % \
+			(self.authz_file, self.access_file, self.userprop_file, self.repositories)
+		
+		self.filename = os.path.join(self.base, 'submin.conf')
+		fp = open(self.filename, 'w')
 		fp.write(conf_data)
 		fp.close()
 		os.environ['SUBMIN_CONF'] = self.filename
@@ -43,13 +39,16 @@ class SingletonTest(unittest.TestCase):
 		fp.close
 
 	def createFiles(self):
-		self.createFile(authz_file, authz_content)
-		self.createFile(access_file, access_content)
-		self.createFile(userprop_file, userprop_content)
+		authz_content = ""
+		access_content = "admin:$1$9BEYXPLN$UtPfDFp1XmyXO0NuMStTP."
+		userprop_content = ""
+
+		self.createFile(self.authz_file, authz_content)
+		self.createFile(self.access_file, access_content)
+		self.createFile(self.userprop_file, userprop_content)
 
 	def tearDown(self):
-		os.unlink(self.filename)
-		os.system('rm -rf %s' % base)
+		os.system('rm -rf %s' % self.base)
 
 	def testInstanceEqual(self):
 		config_instance = Config()
@@ -69,6 +68,53 @@ class SingletonTest(unittest.TestCase):
 	def testGetAttribute(self):
 		config_instance = Config()
 		self.assert_(config_instance.authz)
+
+class IncompleteConfigTest(unittest.TestCase):
+	def setUp(self):
+		self.base = tempfile.mkdtemp()
+
+	def tearDown(self):
+		os.system("rm -rf '%s'" % self.base)
+
+	def writeConfig(self, conf_data):
+		self.filename = os.path.join(self.base, 'submin.conf')
+		fp = open(self.filename, 'w')
+		fp.write(conf_data)
+		fp.close()
+		os.environ['SUBMIN_CONF'] = self.filename
+
+	def testNoConfig(self):
+		os.environ['SUBMIN_CONF'] = '/tmp/non-existing.file'
+		self.assertRaises(CouldNotReadConfig, Config)
+
+	def testNoSvnSection(self):
+		self.writeConfig("")
+		self.assertRaises(MissingConfigData, Config)
+
+	def testNoAuthzOption(self):
+		self.writeConfig("[svn]")
+		self.assertRaises(MissingConfigData, Config)
+
+	def testNoUserPropOption(self):
+		self.writeConfig("[svn]\nauthz_file=%s/submin-unittest-authz" % self.base)
+		self.assertRaises(MissingConfigData, Config)
+
+	def testNoAccessFileOption(self):
+		self.writeConfig("""
+[svn]
+authz_file=%(base)s/submin-unittest-authz
+userprop_file=%(base)s/submin-unittest-userprop
+""" % {'base': self.base})
+		self.assertRaises(MissingConfigData, Config)
+
+	def testNoWWWSection(self):
+		self.writeConfig("""
+[svn]
+authz_file=%(base)s/submin-unittest-authz
+userprop_file=%(base)s/submin-unittest-userprop
+access_file=%(base)s/submin-unittest-access-file
+""" % {'base': self.base})
+		self.assertRaises(MissingConfigData, Config)
 
 if __name__ == "__main__":
 	unittest.main()
