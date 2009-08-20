2914b633669e3f6f031990eeb8c23774601f7202
Michiel Holtkamp <git@supermind.nl>
2009-08-20 17:46:43 +0000

    Removed old submin-admin, renamed '2' to submin-admin
    git-svn-id: https://ssl.supermind.nl/collab/svn/submin/submin/branches/sql@967 9dfea958-5e0b-0410-aa7e-b09452dc4efc

--- bin/submin-admin.py
+++ bin/submin-admin.py
@@ -1,25 +1,28 @@
 #!/usr/bin/python
+import os
 
-def main():
-	from sys import argv, path
-	import os
-	path.append('_SUBMIN_LIB_DIR_')
-	env = 'SUBMIN_LIB_DIR'
-	if env in os.environ:
-		path.append(os.environ[env])
+class App(object):
+	def __init__(self):
+		self.run()
 
-	try:
-		from subminadmin.subminadmin import SubminAdmin
-	except ImportError, e:
-		print e
-		print "is environment %s set?" % env
-		return
+	def systemlibdir(self):
+		import inspect
 
-	try:
-		a = SubminAdmin()
-		a.run(argv)
-	except KeyboardInterrupt:
-		print
+		basefile = inspect.getmodule(self).__file__
+		# Basefile will contain <basedir>/bin/submin_admin.py
+		basedir_bin = os.path.dirname(basefile)
+		basedir = os.path.dirname(basedir_bin)
+		return os.path.join(basedir, 'lib')
+
+	def run(self):
+		from sys import argv, path
+
+		path.append(self.systemlibdir())
+
+		from subminadmin import SubminAdmin
+
+		sa = SubminAdmin(argv)
+		sa.run()
 
 if __name__ == "__main__":
-	main()
+	a = App()
