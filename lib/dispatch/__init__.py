09aa619b9e71a528195976eac807b70e2363e285
Michiel Holtkamp <git@supermind.nl>
2009-10-26 22:50:35 +0000

    Fixed handling of Exceptions for the ajax handlers
    git-svn-id: https://ssl.supermind.nl/collab/svn/submin/submin/trunk@1120 9dfea958-5e0b-0410-aa7e-b09452dc4efc

--- lib/dispatch/__init__.py
+++ lib/dispatch/__init__.py
@@ -63,8 +63,7 @@ def dispatcher(request):
 				details = '<pre>' + details + '</pre>'
 				response = ErrorResponse(str(e), request=request, details=details)
 			else:
-				list = ['<err>' + x + '</err>' for x in list]
-				details = '\n'.join(details)
+				details = '<err>' + details + '</err>'
 				response = XMLStatusResponse('', False, str(e) + details)
 
 	else:
