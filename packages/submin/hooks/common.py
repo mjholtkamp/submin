e36989f56f5eb7a1b1185d87a3951647c2284c25
Jean-Paul van Oosten <submin@jpvanoosten.nl>
2010-02-24 19:29:03 +0000

    Make distinction between system and user hooks
    git-svn-id: https://ssl.supermind.nl/collab/svn/submin/submin/trunk@1176 9dfea958-5e0b-0410-aa7e-b09452dc4efc

--- packages/submin/hooks/common.py
+++ packages/submin/hooks/common.py
@@ -6,24 +6,24 @@ from submin.models.options import Options
 
 def trigger_hook(event, **args):
 	#log(event, **args)
+	# call our own hooks
 
+	trigger_user_hook(event, **args)
+
+def trigger_user_hook(event, **args):
 	o = Options()
-	system_hooks_path = o.static_path("hooks") + "submin" + event
 	user_hooks_path = o.env_path() + "hooks" + event
+	if not user_hooks_path.exists():
+		return
 
-	all_hooks = []
-	if system_hooks_path.exists():
-		all_hooks += glob.glob(str(system_hooks_path + "*"))
-	if user_hooks_path.exists():
-		all_hooks += glob.glob(str(user_hooks_path + "*"))
-
-	all_hooks.sort()
+	user_hooks = glob.glob(str(user_hooks_path + "*"))
+	user_hooks.sort()
 
 	cwd = os.getcwd()
 	os.chdir(str(o.env_path()))
 
 	env = dict([(key.upper(), value) for key, value in args.iteritems()])
-	for hook in all_hooks:
+	for hook in user_hooks:
 		try:
 			# XXX What to do with system hooks which are not executable?
 			# (since they are located in the site-packages directory)
