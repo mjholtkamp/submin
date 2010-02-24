8189177183494c91b1d6aec441c2f53ccebc17f5
Jean-Paul van Oosten <submin@jpvanoosten.nl>
2010-02-24 21:12:02 +0000

    Implemented system-hooks and plugin-hooks system
    Plugins can now add a hooks.py with a dictionary hooks containing mapping an
    event to a list of functions.
    
    Hooks not related to a plugin are placed in the dictionary hooks in
    submin.hooks.system_hooks.
    
    User-hooks are still placed in the directory hooks in their environment as
    described in GenericHooks.
    
    Refs #191
    
    
    
    git-svn-id: https://ssl.supermind.nl/collab/svn/submin/submin/trunk@1177 9dfea958-5e0b-0410-aa7e-b09452dc4efc

--- packages/submin/hooks/common.py
+++ packages/submin/hooks/common.py
@@ -7,8 +7,31 @@ from submin.models.options import Options
 def trigger_hook(event, **args):
 	#log(event, **args)
 	# call our own hooks
+	# first compose a list of all hooks
+	from submin.bootstrap import settings
+	from submin.hooks import system_hooks
+
+	hooks = system_hooks.hooks.copy()
+
+	for vcs_plugin in settings.vcs_plugins:
+		plugin_hooks = _get_vcs_plugin_hooks(vcs_plugin)
+		for key in plugin_hooks:
+			hooks[key] = hooks.get(key, []) + plugin_hooks[key]
+
+	# Then execute all hooks
+	if event in hooks:
+		for hook_fn in hooks[event]:
+			hook_fn(**args)
 
 	trigger_user_hook(event, **args)
+
+def _get_vcs_plugin_hooks(plugin):
+	try:
+		hooks_module = __import__("submin.plugins.vcs.%s.hooks" % plugin,
+				globals(), locals(), ["submin.plugins.vcs.%s" % plugin])
+		return hooks_module.hooks.copy()
+	except (KeyError, ImportError):
+		return {}
 
 def trigger_user_hook(event, **args):
 	o = Options()
