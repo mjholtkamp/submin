f6b0edf7c56ab53509a9fd79d96fa423d17aa7d5
Jean-Paul van Oosten <submin@jpvanoosten.nl>
2011-04-08 21:32:01 +0000

    Moved ./packages/submin to ./submin for easier access
    git-svn-id: https://ssl.supermind.nl/collab/svn/submin/submin/trunk@1320 9dfea958-5e0b-0410-aa7e-b09452dc4efc

--- setup.py
+++ setup.py
@@ -7,39 +7,6 @@ import sys
 import glob
 import fnmatch
 
-def is_package(filename):
-	return os.path.isdir(filename) \
-			and os.path.isfile(os.path.join(filename, "__init__.py"))
-
-# Taken from setuptools
-def find_packages(directory, exclude=()):
-	packages = []
-	stack = [(convert_path(directory), '')]
-	while stack:
-		path, prefix = stack.pop(0)
-		for name in os.listdir(path):
-			filename = os.path.join(path, name)
-			if '.' not in name and is_package(filename):
-				packages.append(prefix + name)
-				stack.append((filename, prefix + name + "."))
-
-	for pattern in exclude:
-		packages = [package for package in packages \
-				if not fnmatch.fnmatchcase(package, pattern)]
-	return packages
-
-def find_data_files(directory, exclude_dirs=()):
-	data_files = []
-	for path, dirs, files in os.walk(directory):
-		if any(fnmatch.fnmatchcase(path, pattern) for pattern in exclude_dirs):
-			continue
-		if files:
-			share_name = path.replace(directory.rstrip(os.path.sep),
-					"share/submin", 1) # Hack to force correct install path
-			data_files.append((share_name,
-				[os.path.join(path, name) for name in files]))
-	return data_files
-
 def find_package_data(directory, exclude_dirs=()):
 	data_files = []
 	for path, dirs, files in os.walk(directory):
@@ -63,10 +30,10 @@ setup(name='Submin',
 	author='Michiel Holtkamp, Jean-Paul van Oosten',
 	author_email='submin@webdevel.nl',
 	url='http://www.supermind.nl/submin/',
-	packages=find_packages("packages", exclude=[".svn"]),
-	package_dir={"submin": "packages/submin"},
+	packages=["submin"],
+	package_dir={"submin": "submin"},
 	package_data={
-		"submin": find_package_data("packages/submin/static", ["*.svn*"]),
+		"submin": find_package_data("submin/static", ["*.svn*"]),
 	},
 	data_files=[
 		("share/man/man1", ("man/submin2-admin.1",)),
