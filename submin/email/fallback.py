2c9e9f0addc3600de11ec113ef7aefd7de76e8a0
Michiel Holtkamp <git@supermind.nl>
2012-08-29 13:29:07 +0000

    Fixed another encoding issue when sending e-mails (refs #278)
    git-svn-id: https://ssl.supermind.nl/collab/svn/submin/submin/trunk@1400 9dfea958-5e0b-0410-aa7e-b09452dc4efc

--- submin/email/fallback.py
+++ submin/email/fallback.py
@@ -3,8 +3,9 @@ import smtp
 from submin.models.exceptions import SendEmailError
 
 def sendmail(sender, receiver, message):
+	msg_e = message.encode('utf-8')
 	try:
-		smtp.send(sender, receiver, message)
+		smtp.send(sender, receiver, msg_e)
 	except SendEmailError:
 		# this can still raise SendEmailError
-		local.send(sender, receiver, message)
+		local.send(sender, receiver, msg_e)
