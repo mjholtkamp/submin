db7174476c521df4c3284294bf2faae838cde8fb
Michiel Holtkamp <git@supermind.nl>
2012-11-27 02:22:16 +0000

    Updated tests to comply with the new way of handling url path's
    git-svn-id: https://ssl.supermind.nl/collab/svn/submin/submin/trunk@1420 9dfea958-5e0b-0410-aa7e-b09452dc4efc

--- submin/subminadmin/unittests.py
+++ submin/subminadmin/unittests.py
@@ -20,12 +20,12 @@ class SubminAdminApacheConfTests(unittest.TestCase):
 	def testUrlPathFullURLSubDir(self):
 		ac = c_apacheconf.c_apacheconf(None, [])
 		result = ac.urlpath('http://www.example.com/submin/help')
-		self.assertEquals(result, '/submin/help/')
+		self.assertEquals(result, '/submin/help')
 
 	def testUrlPathURLpath(self):
 		ac = c_apacheconf.c_apacheconf(None, [])
 		result = ac.urlpath('/submin/')
-		self.assertEquals(result, '/submin/')
+		self.assertEquals(result, '/submin')
 
 class SubminAdminElseTests(unittest.TestCase):
 	def testSomethingelse(self):
