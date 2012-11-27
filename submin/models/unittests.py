fca29b50a392001a8722583a53db19be648730a0
Michiel Holtkamp <git@supermind.nl>
2012-11-27 05:05:46 +0000

    Update unittests
    They were not working because they were still based
    on submin1.2 and never updated to submin2.
    
    Also used python-mock instead of pmock, because the first
    is included in python3.3 stdlib.
    
    One test still fails, this is good, because the code that
    it is testing is wrong.
    
    
    
    git-svn-id: https://ssl.supermind.nl/collab/svn/submin/submin/trunk@1422 9dfea958-5e0b-0410-aa7e-b09452dc4efc

--- submin/models/unittests.py
+++ submin/models/unittests.py
@@ -1,13 +1,16 @@
 import os
 import sys
 import unittest
-from pmock import *
+from mock import Mock
 
 mock_settings = Mock()
 mock_settings.storage = "mock"
 
 from submin.bootstrap import setSettings
 setSettings(mock_settings)
+
+from submin.models import repository
+from submin.models import options
 
 model_tests = ["users.UserTests", "groups.GroupTests", "options.OptionTests"]
 
@@ -18,171 +21,146 @@ def suite():
 		["submin.models.tests.%s" % x for x in model_tests]))
 	return s
 
-if False:
-	class RepositoryTests(unittest.TestCase):
-		def setUp(self):
-			import tempfile
-			self.reposdir = tempfile.mkdtemp()
-			self.config_file = tempfile.NamedTemporaryFile(dir="/tmp/", prefix="submin_cfg_")
-			self.authz_file = tempfile.NamedTemporaryFile(dir="/tmp/", prefix="submin_authz_")
-			self.userprop_file = tempfile.NamedTemporaryFile(dir="/tmp/", prefix="submin_userprop_")
-			self.access_file = tempfile.NamedTemporaryFile(dir="/tmp/", prefix="submin_access_file_")
+class RepositoryTests(unittest.TestCase):
+	def setUp(self):
+		import tempfile
+		self.submin_env = tempfile.mkdtemp('submin-unittest')
+		self.conf_dir = os.path.join(self.submin_env, 'conf')
+		os.mkdir(self.conf_dir)
+		self.authz_file = os.path.join(self.submin_env, 'conf', 'authz')
 
-			os.environ['SUBMIN_CONF'] = self.config_file.name
-			config_content = """
-	[svn]
-	authz_file = %s
-	userprop_file = %s
-	access_file = %s
-	repositories = %s
+		os.environ['SUBMIN_ENV'] = self.submin_env
+		mock_settings.base_dir = self.submin_env
+		options.set_value('vcs_plugins', 'svn')
+		options.set_value('svn_dir', 'svn')
+		options.set_value('trac_dir', 'trac')
+		options.set_value('svn_authz_file', 'conf/authz')
+		options.set_value('enabled_trac', 'no')
 
-	[www]
-	base_url = /
+		self.svn_dir = str(options.env_path('svn_dir'))
+		self.trac_dir = str(options.env_path('trac_dir'))
+		os.mkdir(self.svn_dir)
+		os.mkdir(self.trac_dir)
 
-	[backend]
-	bindir = /bin
+		# now make some repositories
+		self.repositories = [
+			{'name': 'foo', 'status': 'ok', 'vcs': 'svn'},
+			{'name': 'invalidperm', 'status': 'permission denied', 'vcs': 'svn'},
+			{'name': 'invalidperm2', 'status': 'permission denied', 'vcs': 'svn'},
+			{'name': 'subdirs', 'status': 'ok', 'vcs': 'svn'},
+		]
+		for r in self.repositories:
+			os.system("svnadmin create '%s'" % os.path.join(self.svn_dir, r['name']))
 
-			""" % (self.authz_file.name, self.userprop_file.name, \
-				self.access_file.name, self.reposdir)
+		os.system("chmod 000 '%s'" % \
+			os.path.join(self.svn_dir, 'invalidperm'))
 
-			self.config_file.write(config_content)
-			self.config_file.flush() # but keep it open!
+		os.system("chmod 000 '%s'" % \
+			os.path.join(self.svn_dir, 'invalidperm2', 'db', 'revs'))
 
-			# this is so config loads the new config file, it's a Singleton!
-			Config().reinit()
+	def tearDown(self):
+		os.system("chmod 777 '%s'" % os.path.join(self.svn_dir, 'invalidperm'))
+		os.system("chmod 777 '%s'" % \
+			os.path.join(self.svn_dir, 'invalidperm2', 'db', 'revs'))
+		os.system("rm -rf '%s'" % self.submin_env)
 
-			# now make some repositories
-			self.repositories = ['foo', 'BAR', 'removeme', 'invalidperm', \
-				'invalidperm2', 'subdirs']
-			for r in self.repositories:
-				os.system("svnadmin create '%s'" % os.path.join(self.reposdir, r))
+	def testRepositoriesOnDisk(self):
+		result = repository.Repository.list_all()
+		result.sort()
+		copy = self.repositories[:]
+		copy.sort()
+		self.assertEquals(result, copy)
 
-			os.system("chmod 000 '%s'" % \
-				os.path.join(self.reposdir, 'invalidperm'))
+	def testListRepositoriesAll(self):
+		"""Test listRepositories, which checks for valid permissions of repositories"""
+		self.assertEquals("", "This test fails because list is not filtering correctly")
+		mock_user = Mock()
+		mock_user.is_admin = False
 
-			os.system("chmod 000 '%s'" % \
-				os.path.join(self.reposdir, 'invalidperm2', 'db', 'revs'))
+		result = repository.Repository.list(mock_user)
+		copy = self.repositories[:]
+		copy = [d for d in self.repositories if d.get('name') == 'foo' or d.get('name') == 'subdirs']
+		copy.sort()
+		self.assertEquals(result, copy)
 
-		def tearDown(self):
-			for f in [self.config_file, self.authz_file, self.userprop_file, self.access_file]:
-				f.close()
+	def testExistingRepository(self):
+		r = repository.Repository('foo', 'svn')
+		self.assertEquals(r.name, 'foo')
 
-			os.system("chmod 777 '%s'" % os.path.join(self.reposdir, 'invalidperm'))
-			os.system("chmod 777 '%s'" % \
-				os.path.join(self.reposdir, 'invalidperm2', 'db', 'revs'))
-			os.system("rm -rf '%s'" % self.reposdir)
+	def testInvalidPermRepository(self):
+		self.assertRaises(repository.PermissionError, repository.Repository, "invalidperm", "svn")
 
-		def testRepositoriesOnDisk(self):
-			result = repositoriesOnDisk()
-			result.sort()
-			copy = self.repositories[:]
-			copy.sort()
-			self.assertEquals(result, copy)
+	def testInvalidPermRepository2(self):
+		self.assertRaises(repository.PermissionError, repository.Repository, "invalidperm2", "svn")
 
-		def testListRepositoriesAll(self):
-			"""Test listRepositories, which checks for valid permissions of repositories"""
-			mock_user = Mock()
-			mock_user.is_admin = True
+	def testUnknownRepository(self):
+		self.assertRaises(repository.DoesNotExistError, repository.Repository, "non-existing-repository", "svn")
 
-			result = listRepositories(mock_user, only_invalid=False)
-			copy = self.repositories[:]
-			copy.sort()
-			copy.remove('invalidperm')
-			copy.remove('invalidperm2')
-			self.assertEquals(result, copy)
+	def testHasSubDirs(self):
+		for subdir in ['test', 'test/subdir']:
+			os.system("svn mkdir -m '' file://'%s' >/dev/null" % \
+				os.path.join(self.svn_dir, 'subdirs', subdir))
+		r = repository.Repository('subdirs', 'svn')
+		subdirs = r.subdirs('test')
+		expected = [{'has_subdirs': False, 'name': u'subdir'}]
+		self.assertEquals(subdirs, expected)
 
-		def testListRepositoriesOnlyInvalid(self):
-			"""Test listRepositories, which checks for valid permissions of repositories"""
-			mock_user = Mock()
-			mock_user.is_admin = True
-			result = listRepositories(mock_user, only_invalid=True)
-			expected_result = ['invalidperm', 'invalidperm2']
-			self.assertEquals(result, expected_result)
+	def testSubDirsContents(self):
+		for subdir in ['test', 'test/subdir', 'nosubdirs']:
+			os.system("svn mkdir -m '' file://'%s' >/dev/null" % \
+				os.path.join(self.svn_dir, 'subdirs', subdir))
+		r = repository.Repository('subdirs', 'svn')
+		result = r.subdirs('')
+		expected_result = [{'has_subdirs': False, 'name': u'nosubdirs'}, \
+			{'name': u'test', 'has_subdirs': True}]
 
-		def testExistingRepository(self):
-			r = Repository('foo')
-			self.assertEquals(str(r), 'foo')
+		result.sort()
+		expected_result.sort()
+		self.assertEquals(result, expected_result)
 
-		def testInvalidPermRepository(self):
-			self.assertRaises(Repository.PermissionDenied, Repository, "invalidperm")
+	def testRemoveRepository(self):
+		r = repository.Repository('subdirs', 'svn')
+		r.remove()
+		self.assertRaises(repository.DoesNotExistError, repository.Repository, 'subdirs', 'svn')
 
-		def testInvalidPermRepository2(self):
-			self.assertRaises(Repository.PermissionDenied, Repository, "invalidperm2")
+	def testChangeNotificationsEmptyHook(self):
+		expected_hook = '''#!/bin/sh
+### SUBMIN AUTOCONFIG, DO NOT ALTER FOLLOWING LINE ###
+/usr/bin/python /bin/post-commit.py "%s" "$1" "$2"
+''' % self.submin_env
+		hook_fname = os.path.join(self.svn_dir, 'foo', 'hooks', 'post-commit')
 
-		def testUnknownRepository(self):
-			self.assertRaises(Repository.DoesNotExist, Repository, "non-existing-repository")
+		r = repository.Repository('foo', 'svn')
+		r.enableCommitEmails(enable=True)
+		hook = ''.join(file(hook_fname, 'r').readlines())
+		self.assertTrue('### SUBMIN AUTOCONFIG, DO NOT ALTER FOLLOWING LINE ###' in hook)
+		self.assertTrue('mailer.py.conf' in hook)
 
-		def testHasSubDirs(self):
-			for subdir in ['test', 'test/subdir']:
-				os.system("svn mkdir -m '' file://'%s' >/dev/null" % \
-					os.path.join(self.reposdir, 'subdirs', subdir))
-			r = Repository('subdirs')
-			self.assertTrue(r.hassubdirs('test'))
+	def testChangeNotificationsExistingHook(self):
+		expected_hook = '''#!/bin/sh
+# just a comment
+'''
+		hook_fname = os.path.join(self.svn_dir, 'foo', 'hooks', 'post-commit')
+		file(hook_fname, 'w').write(expected_hook)
 
-		def testSubDirsContents(self):
-			for subdir in ['test', 'test/subdir', 'nosubdirs']:
-				os.system("svn mkdir -m '' file://'%s' >/dev/null" % \
-					os.path.join(self.reposdir, 'subdirs', subdir))
-			r = Repository('subdirs')
-			result = r.getsubdirs('')
-			expected_result = [{'has_subdirs': False, 'name': u'nosubdirs'}, \
-				{'name': u'test', 'has_subdirs': True}]
+		r = repository.Repository('foo', 'svn')
+		r.enableCommitEmails(enable=True)
+		hook = ''.join(file(hook_fname, 'r').readlines())
+		self.assertTrue('# just a comment' in hook)
+		self.assertTrue('mailer.py.conf' in hook)
+		r.enableCommitEmails(enable=False)
+		hook = ''.join(file(hook_fname, 'r').readlines())
+		self.assertEquals(hook, expected_hook)
 
-			result.sort()
-			expected_result.sort()
-			self.assertEquals(result, expected_result)
-
-		def testRemoveRepository(self):
-			r = Repository('removeme')
-			r.remove()
-			result = repositoriesOnDisk()
-			copy = self.repositories[:]
-			for res in result:
-				copy.remove(res)
-
-			self.assertEquals(['removeme'], copy)
-
-		def testChangeNotificationsEmptyHook(self):
-			expected_hook = '''#!/bin/sh
-	### SUBMIN AUTOCONFIG, DO NOT ALTER FOLLOWING LINE ###
-	/usr/bin/python /bin/post-commit.py "%s" "$1" "$2"
-	''' % self.config_file.name
-			hook_fname = os.path.join(self.reposdir, 'BAR', 'hooks', 'post-commit')
-
-			r = Repository('BAR')
-			r.changeNotifications(enable=True)
-			hook = ''.join(file(hook_fname, 'r').readlines())
-			self.assertEquals(hook, expected_hook)
-
-		def testChangeNotificationsExistingHook(self):
-			expected_hook1 = '''#!/bin/sh
-	# just a comment
-	### SUBMIN AUTOCONFIG, DO NOT ALTER FOLLOWING LINE ###
-	/usr/bin/python /bin/post-commit.py "%s" "$1" "$2"
-	''' % self.config_file.name
-			expected_hook2 = '''#!/bin/sh
-	# just a comment
-	'''
-			hook_fname = os.path.join(self.reposdir, 'BAR', 'hooks', 'post-commit')
-			file(hook_fname, 'w').write(expected_hook2)
-
-			r = Repository('BAR')
-			r.changeNotifications(enable=True)
-			hook = ''.join(file(hook_fname, 'r').readlines())
-			self.assertEquals(hook, expected_hook1)
-			r.changeNotifications(enable=False)
-			hook = ''.join(file(hook_fname, 'r').readlines())
-			self.assertEquals(hook, expected_hook2)
-
-		def testNotificationsEnabled(self):
-			r = Repository('BAR')
-			# first time, because no file is present
-			self.assertFalse(r.notificationsEnabled())
-			r.changeNotifications(enable=True)
-			self.assertTrue(r.notificationsEnabled())
-			# a second time, because now a file is created
-			r.changeNotifications(enable=False)
-			self.assertFalse(r.notificationsEnabled())
+	def testNotificationsEnabled(self):
+		r = repository.Repository('foo', 'svn')
+		# first time, because no file is present
+		self.assertFalse(r.commitEmailsEnabled())
+		r.enableCommitEmails(enable=True)
+		self.assertTrue(r.commitEmailsEnabled())
+		# a second time, because now a file is created
+		r.enableCommitEmails(enable=False)
+		self.assertFalse(r.commitEmailsEnabled())
 
 if __name__ == "__main__":
 	unittest.main()
