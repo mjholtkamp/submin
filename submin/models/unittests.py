1aa1a47315eac92122d1b011498d4ed5272404e5
Michiel Holtkamp <git@supermind.nl>
2012-12-06 05:12:48 +0000

    Instead of hardcoding the list of test classes (and possibly missing future tests), generate them from the files found in tests/*.py
    git-svn-id: https://ssl.supermind.nl/collab/svn/submin/submin/trunk@1445 9dfea958-5e0b-0410-aa7e-b09452dc4efc

--- submin/models/unittests.py
+++ submin/models/unittests.py
@@ -1,11 +1,27 @@
+import os
+import sys
+import re
+import inspect
+
 import unittest
 
-model_tests = ["users.UserTests", "groups.GroupTests", "options.OptionTests",
-			"trac.TracTests", "repository.RepositoryTests"]
+def find_unittest_modules():
+	tests_dir = os.path.join(os.path.dirname(__file__), 'tests')
+	for (path, dirs, files) in os.walk(tests_dir):
+		for f in files:
+			for match in re.findall('^(.*)\.py$', f):
+				if match == "__init__":
+					continue
+
+				module_path = "submin.models.tests.%s" % (match,)
+				module = __import__(module_path)
+				for clsname, cls in inspect.getmembers(sys.modules[module_path], inspect.isclass):
+					if cls.__module__ == module_path and clsname.endswith('Tests'):
+						yield "%s.%s" % (module_path, clsname)
 
 def suite():
 	s = unittest.TestSuite()
-	subtests = ["submin.models.tests.%s" % x for x in model_tests]
+	subtests = find_unittest_modules()
 	map(s.addTest, map(unittest.defaultTestLoader.loadTestsFromName, subtests))
 	return s
 
