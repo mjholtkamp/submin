5c07ad82ab63c7ef7a97d1320538d3a9355b10c7
Michiel Holtkamp <git@supermind.nl>
2013-03-02 09:18:15 +0000

    Somehow, this line was copied, but without the ssh-export, causing it to overwrite the previous entry, which in turned caused the 'export_ssh_keys' never to be called (fixes #302)
    git-svn-id: https://ssl.supermind.nl/collab/svn/submin/submin/trunk@1525 9dfea958-5e0b-0410-aa7e-b09452dc4efc

--- submin/plugins/vcs/git/hooks.py
+++ submin/plugins/vcs/git/hooks.py
@@ -3,7 +3,6 @@ from submin.plugins.vcs.git.export import export_ssh_keys, export_notifications
 hooks = {
 	'user-update': [export_ssh_keys, export_notifications],
 	'user-delete': [export_notifications],
-	'user-update': [export_notifications],
 	'user-notifications-update': [export_notifications],
 	'group-add-member': [export_notifications],
 	'group-delete-member': [export_notifications],
