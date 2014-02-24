929a5d9e35e8e7fd8ab1fc2dff4c3910f9192279
Michiel Holtkamp <git@elfstone.nl>
2014-02-24 22:29:38 +0000

    Added tests for subminadmin c_config

--- submin/subminadmin/unittests.py
+++ submin/subminadmin/unittests.py
@@ -1,11 +1,26 @@
 import sys
 import unittest
 import c_quit
+import c_config
 import c_help
 import os
 import tempfile
+import shutil # to remove temp dir
 from mock import Mock, MagicMock
 from StringIO import StringIO
+
+mock_settings = Mock()
+mock_settings.storage = "sql"
+mock_settings.sqlite_path = ":memory:"
+mock_settings.base_dir = "/"
+
+from submin.bootstrap import setSettings
+setSettings(mock_settings)
+
+from submin.path.path import Path
+from submin.models import storage
+from submin.models import options
+from submin.models.exceptions import UnknownKeyError
 
 class SubminAdminApacheConfTests(unittest.TestCase):
 	@classmethod
@@ -61,6 +76,75 @@ class SubminAdminQuit(unittest.TestCase):
 		env.quit = False
 		q = c_quit.c_quit(env, [])
 		self.assertRaises(SystemExit, q.run())
+
+class SubminAdminConfig(unittest.TestCase):
+	def setUp(self):
+		self.submin_env = Path(tempfile.mkdtemp(prefix='submin-unittest'))
+		conf_dir = self.submin_env + 'conf'
+		os.mkdir(conf_dir)
+		mock_settings.base_dir = self.submin_env
+		storage.open(mock_settings)
+		storage.database_evolve()
+		self.sa = MagicMock()
+		self.sa.env = self.submin_env
+		self.saved_stdout, sys.stdout = sys.stdout, StringIO()
+
+	def tearDown(self):
+		storage.close()
+		shutil.rmtree(self.submin_env)
+		sys.stdout = self.saved_stdout
+
+	def testSetOption(self):
+		c = c_config.c_config(self.sa, ['set', 'answer', '42'])
+		c.run()
+		self.assertEquals(options.value('answer'), u'42')
+
+	def testGetOption(self):
+		options.set_value('answer', '42')
+		c = c_config.c_config(self.sa, ['get', 'answer'])
+		c.run()
+		self.assertTrue('42' in sys.stdout.getvalue())
+
+	def testGetUnknownOption(self):
+		c = c_config.c_config(self.sa, ['get', 'answer'])
+		c.run()
+		self.assertEquals('ERROR: answer does not exist\n', sys.stdout.getvalue())
+
+	def testGetAllOptions(self):
+		options.set_value('answer', '42')
+		c = c_config.c_config(self.sa, ['get'])
+		c.run()
+		output = sys.stdout.getvalue()
+		self.assertTrue('database_version' in output)
+		self.assertTrue('git_dir' in output)
+		self.assertTrue('42' in output)
+
+	def testDefaults(self):
+		expected = [(u'database_version', u'9'), (u'auth_type', u'sql'),
+				(u'svn_dir', u'svn'), (u'git_dir', u'git'),
+				(u'env_path', u'/bin:/usr/bin:/usr/local/bin:/opt/local/bin'),
+				(u'dir_bin', u'static/bin'),(u'base_url_trac', u'/trac'),
+				(u'base_url_git', u'/git'), (u'base_url_submin', u'/submin'),
+				(u'http_vhost', u'darim'), (u'enabled_trac', u'no'),
+				(u'trac_dir', u'trac'), (u'vcs_plugins', u'svn'),
+				(u'base_url_svn', u'/svn')]
+		unpredictable_keys = [u'session_salt']
+		c = c_config.c_config(self.sa, ['defaults'])
+		c.run()
+
+		observed = options.options()
+		for idx, opt in enumerate(observed):
+			key, value = opt
+			if key in unpredictable_keys:
+				del observed[idx]
+
+		self.assertEquals(expected, observed)
+
+	def testUnsetOption(self):
+		options.set_value('answer', '42')
+		c = c_config.c_config(self.sa, ['unset', 'answer'])
+		c.run()
+		self.assertRaises(UnknownKeyError, options.value, 'answer')
 
 class SubminAdminHelp(unittest.TestCase):
 	def _cmd_instance(self, string, array, print_error=False):
