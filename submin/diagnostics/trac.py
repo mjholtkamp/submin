mjholtkamp/submin

commit 992641b46f3578c16c56c7523ada55f10eaeac17

Show disabled components as disabled, not as warn (fixes #398)

submin/diagnostics/trac.py
@@ -14,8 +14,8 @@ from .common import add_labels
 
 class SyncError(Exception):
 	pass
-
-warnings = ['enabled_trac', 'trac_acl_hook', 'trac_envs_all_connected',
+disabled = ['enabled_trac']
+warnings = ['trac_acl_hook', 'trac_envs_all_connected',
 		'trac_htpasswd_file']
 fails = ['installed_trac', 'trac_dir_set', 'trac_sync_access',
 		'trac_sync_access', 'trac_envs_complete', 'trac_htpasswd_dir_exists']
@@ -25,7 +25,9 @@ def diagnostics():
 	results['enabled_trac'] = options.value('enabled_trac', 'no') != 'no'
 
 	if not results['enabled_trac']:
-		return add_labels(results, 'trac_all', warnings, fails)
+		results['enabled_trac_label'] = 'disabled'
+		results['trac_all_label'] = 'disabled'
+		return results
 
 	results['installed_trac'] = trac.has_trac_admin()
 

submin/diagnostics/git.py
@@ -19,7 +19,9 @@ def diagnostics():
 	results['enabled_git'] = 'git' in options.value('vcs_plugins', '')
 
 	if not results['enabled_git']:
-		return add_labels(results, 'git_all', warnings, fails)
+		results['enabled_git_label'] = 'disabled'
+		results['git_all_label'] = 'disabled'
+		return results
 
 	try:
 		git_dir = options.env_path('git_dir')

submin/diagnostics/svn.py
@@ -12,7 +12,9 @@ def diagnostics():
 	results['enabled_svn'] = 'svn' in options.value('vcs_plugins', '')
 
 	if not results['enabled_svn']:
-		return add_labels(results, 'svn_all', warnings, fails)
+		results['enabled_svn_label'] = 'disabled'
+		results['svn_all_label'] = 'disabled'
+		return results
 
 	try:
 		svn_dir = options.env_path('svn_dir')

submin/static/www/css/master.css
@@ -215,6 +215,17 @@ div.c_icon.minus {
 .diag_warn:before {
 	content: 'Warn';
 }
+.diag_disabled {
+	color: lightgray;
+	padding: 0 0.2em;
+	border-radius: 4px;
+	-moz-border-radius: 4px;
+	-webkit-border-radius: 4px;
+}
+.diag_disabled:before {
+	content: 'Disabled';
+}
+
 
 .diag_inline {
 	float: right;

commit af70befcbb5435b52178e3a759ba55a50d5c6e2f

Diagnostics: fixed trac=OK (refs #398)
Fixed bug introduced in [992641b46f3578c16c56c7523ada55f10eaeac17]:
Somehow I missed checking the code before commiting, there was a
piece of prototype code/data left.

submin/diagnostics/trac.py
@@ -14,8 +14,7 @@ from .common import add_labels
 
 class SyncError(Exception):
 	pass
-disabled = ['enabled_trac']
-warnings = ['trac_acl_hook', 'trac_envs_all_connected',
+warnings = ['enabled_trac', 'trac_acl_hook', 'trac_envs_all_connected',
 		'trac_htpasswd_file']
 fails = ['installed_trac', 'trac_dir_set', 'trac_sync_access',
 		'trac_sync_access', 'trac_envs_complete', 'trac_htpasswd_dir_exists']