ee05c523091cebaafcd533458cfcfab657110d15
Michiel Holtkamp <git@elfstone.nl>
2014-08-04 23:00:09 +0200

    Split up command-line diagnostics into categories
    The diagnostics command can now be run separately on the command line. For
    example, to only run the email diagnostics, the following command can be
    used:
    
      submin2-admin <submin env> diagnostics email

--- submin/subminadmin/c_diagnostics.py
+++ submin/subminadmin/c_diagnostics.py
@@ -1,9 +1,18 @@
 class c_diagnostics():
-	'''Run diagnostics on submin'''
+	'''Run diagnostics on submin
+Usage:
+    diagnostics [<type>]  - run diagnostics of type <type>
+
+    Where <type> is one of: 'all', 'email', 'git', 'svn', 'trac' or nothing.
+    When the <type> is 'all' or nothing, all diagnostics are run, otherwise
+    only the selected category is run (e.g. 'diagnostics email' to only run
+    email diagnostics. When all diagnostics are run, a summary is given at
+    the end.'''
 	needs_env = True
 
 	def __init__(self, sa, argv):
 		self.sa = sa
+		self.argv = argv
 
 	def run(self):
 		from submin.models import options
@@ -13,12 +22,21 @@ class c_diagnostics():
 		localvars = {}
 
 		diagnostics = {}
-		diagnostics.update(trac.diagnostics())
-		diagnostics.update(svn.diagnostics())
-		diagnostics.update(git.diagnostics())
-		diagnostics.update(email.diagnostics())
+		if len(self.argv) > 0:
+			which = self.argv[0]
+		else:
+			which = 'all'
+
+		if which in ('all', 'email'):
+			diagnostics.update(email.diagnostics())
+		if which in ('all', 'git'):
+			diagnostics.update(git.diagnostics())
+		if which in ('all', 'svn'):
+			diagnostics.update(svn.diagnostics())
+		if which in ('all', 'trac'):
+			diagnostics.update(trac.diagnostics())
 		localvars['diag'] = diagnostics
 		localvars['subminenv'] = options.env_path()
 
-		formatted = evaluate('diagnostics.print', localvars)
+		formatted = evaluate('diagnostics.%s.print' % (which,), localvars)
 		print(formatted)
